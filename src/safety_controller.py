487dd6a96dd14a55ffb08a963339c9a30f888e06
naianis <35470193+naianis@users.noreply.github.com>
2020-06-30 18:09:18 -0300

    Update safety_controller.py

--- src/safety_controller.py
+++ src/safety_controller.py
@@ -7,16 +7,24 @@ from visual_path_husky.msg import SysStatus
 from nav_msgs.msg import Odometry
 from std_msgs.msg import Bool, Int32
 
-def main():
-    rospy.init_node('safety_controller')
-    rate = rospy.Rate(3)
 
-    scan_message = LaserScan()
-    status = SysStatus()
+class SafetyController(object):
 
+    def __init__(self):
+        self.safety_publisher = rospy.Publisher('safety_controller', SysStatus, queue_size=5)
+        self.sub = rospy.Subscriber('/scan', LaserScan, self.scanCallback )
+        self.circle_feedback = rospy.Subscriber('/circle_control', Bool, self.circleCallback)
+        self.line_feedback = rospy.Subscriber('/line_control', Bool, self.lineCallback)
+
+        self.scan_message = LaserScan()
+        self.status = SysStatus()
+        self.circle_feedback_message = Bool()
+        self.line_feedback_message = Bool()
+
+        self.circle_feedback_message = False
 
     #calculates the estimated size of the obstacle
-    def obstacleSize(scan_message, first_index):
+    def obstacleSize(self, scan_message, first_index):
         i = first_index
 
         while scan_message.ranges[i] < 5 and i < 720:
@@ -26,8 +34,7 @@ def main():
         angle = (last_index - first_index) * scan_message.angle_increment
         #cos law
         virtual_size = math.sqrt (pow(scan_message.ranges[first_index],2) + pow(scan_message.ranges[last_index],2) - 2 * scan_message.ranges[first_index] * scan_message.ranges[last_index] * math.cos(angle))
-        status.obstacle_size = virtual_size
-        safety_publisher.publish(status)
+        self.status.obstacle_size = virtual_size
         return virtual_size
         '''
         print("first index: ") + str(first_index) + ("last index: ") + str(last_index)
@@ -37,31 +44,49 @@ def main():
         '''
 
     #get position of the robot when an obstacle is found
-    def getPosition(flag_keeper):
+    def getPosition(self, flag_keeper):
 
-        if flag_keeper != status.flag :
+        if flag_keeper != self.status.flag :
+            print("getting position")
             position = rospy.wait_for_message('/odometry/filtered', Odometry)
 
             quaternion = (position.pose.pose.orientation.x, position.pose.pose.orientation.y, position.pose.pose.orientation.z, position.pose.pose.orientation.w)
             euler = tf.transformations.euler_from_quaternion(quaternion)
 
-            status.initial_position[0] = position.pose.pose.position.x
-            status.initial_position[1] = position.pose.pose.position.y
-            status.initial_position[2] = euler[2]
+            self.status.initial_position = []
+            self.status.initial_position.append(position.pose.pose.position.x)
+            self.status.initial_position.append(position.pose.pose.position.y)
+            self.status.initial_position.append( euler[2])
 
-            flag_keeper = status.flag
+            print (self.status.initial_position)
+
+            flag_keeper = self.status.flag
+
+    def circleCallback(self, circle_msg):
+        self.circle_feedback_message = circle_msg.data
+
+        if circle_msg.data:
+            self.status.flag = True
+
+
+    def lineCallback(self, line_msg):
+        self.line_feedback_message = line_msg.data
+        self.status.found_line = line_msg.data
+        self.safety_publisher.publish(self.status)
 
 
 
-    def scanCallback(scan_data):
+
+
+
+    def scanCallback(self, scan_data):
         if len(scan_data.ranges) < 270: # those reading are somehow wrong
             return
 
         else:
             global flag_keeper
-            flag_keeper = status.flag
-            angle_min = -math.pi/2
-            angle_max = math.pi/2
+            flag_keeper = self.status.flag
+            angle_max = math.pi/6
 
             global readings_filter
             readings_filter = Int32()
@@ -69,28 +94,37 @@ def main():
             #calculates the index of data of interest
             index = int (angle_max / scan_data.angle_increment)
             size = len(scan_data.ranges)
-            status.flag = False
+            self.status.flag = False
 
             # search obstacles between angles of interest
 
             for i in range(index*2):
                 current_index = int(size/2 - index + i)
                 if scan_data.ranges[current_index] < 1.5 and scan_data.ranges[current_index] > 0.1:
-                    size = obstacleSize(scan_data, current_index)
+                    size = self.obstacleSize(scan_data, current_index)
                     if(size > 0.1):
-                        status.flag = True
-                        getPosition(flag_keeper)
-                        obstacleSize(scan_data, current_index)
+                        self.status.flag = True
+                        self.getPosition(flag_keeper)
+                        self.obstacleSize(scan_data, current_index)
+                        self.status.distance_to_obstacle = scan_data.ranges[current_index]
+                        rospy.loginfo("Obstacle found")
                         break
 
-            status.distance_to_obstacle = scan_data.ranges[360] # This reading must be improved. Do not work properly yet
-            safety_publisher.publish(status)
+
+            if self.circle_feedback_message:
+                rospy.loginfo("Circle Flag activated")
+                self.status.flag = True
+
+            self.safety_publisher.publish(self.status)
 
 
 
-    safety_publisher = rospy.Publisher('safety_controller', SysStatus, queue_size=5)
-    sub = rospy.Subscriber('/scan', LaserScan, scanCallback )
+def main():
 
+    rospy.init_node('safety_controller')
+    rate = rospy.Rate(3)
+
+    control_obj = SafetyController()
     rospy.spin()
 
 if __name__ == '__main__':
